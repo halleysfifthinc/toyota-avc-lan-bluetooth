c5b42d8454f64f090b2315baee3c15f205ae1556
unknown <Ryan@Ryan-PC.(none)>
2012-07-17 21:29:45 -0600

    Added Licence info

--- code/src/bluetooth.h
+++ code/src/bluetooth.h
@@ -1,3 +1,20 @@
+	/*
+	This file is part of toyota-avc-lan-bluetooth.
+
+    toyota-avc-lan-bluetooth is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    toyota-avc-lan-bluetooth is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with toyota-avc-lan-bluetooth.  If not, see <http://www.gnu.org/licenses/>.
+	*/
+
 #ifndef __BLUETOOTH_H
 #define __BLUETOOTH_H
 
